3698c2d9cbdc0f14f6572d0a222331e858ba39ba
Benjamin Bannier <bbannier@gmail.com>
2026-08-24 12:01:27 +0200

    Drop dependency on deprecated `proc-macro-error2` crate
    We can implement the functionality it provided with plain `syn::Error`.

--- crates/zeek-websocket-derive/src/lib.rs
+++ crates/zeek-websocket-derive/src/lib.rs
@@ -1,4 +1,3 @@
-use proc_macro_error2::{abort, proc_macro_error};
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 use syn::{Data, DeriveInput, Field, Fields, Ident, Type, parse_macro_input, spanned::Spanned};
@@ -130,101 +129,125 @@ use syn::{Data, DeriveInput, Field, Fields, Ident, Type, parse_macro_input, span
 /// assert!(x.is_err());
 /// ```
 ///
-#[proc_macro_error]
 #[proc_macro_derive(ZeekType)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
+    match derive_impl(&ast) {
+        Ok(tokens) => tokens.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
+}
 
+fn derive_impl(ast: &DeriveInput) -> syn::Result<TokenStream> {
     let name = &ast.ident;
 
-    let Data::Struct(struct_) = &ast.data else {
-        abort!(ast.span(), "only structs can derive ZeekType");
+    let struct_ = match &ast.data {
+        Data::Struct(s) => s,
+        Data::Enum(e) => {
+            return Err(syn::Error::new(
+                e.enum_token.span,
+                "only structs can derive ZeekType",
+            ));
+        }
+        Data::Union(u) => {
+            return Err(syn::Error::new(
+                u.union_token.span,
+                "only structs can derive ZeekType",
+            ));
+        }
     };
 
     let Fields::Named(fields) = &struct_.fields else {
-        abort!(
-            ast.span(),
-            "only structs with named fields can derive ZeekType"
-        );
+        return Err(syn::Error::new(
+            struct_.struct_token.span,
+            "only structs with named fields can derive ZeekType",
+        ));
     };
     let fields: Vec<_> = fields.named.iter().collect();
 
-    let value_from = impl_value_from(name, &fields);
-    let from_value = impl_from_value(name, &fields);
+    let value_from = impl_value_from(name, &fields)?;
+    let from_value = impl_from_value(name, &fields)?;
 
-    quote! {
+    Ok(quote! {
         #value_from
 
         #from_value
-    }
-    .into()
+    })
 }
 
-fn impl_value_from(name: &Ident, fields: &[&Field]) -> TokenStream {
-    let fields = fields.iter().map(|f| {
-        let Some(field_name) = &f.ident else {
-            abort!(f.span(), "unsupported field name");
-        };
+fn impl_value_from(name: &Ident, fields: &[&Field]) -> syn::Result<TokenStream> {
+    let fields = fields
+        .iter()
+        .map(|f| {
+            let field_name = f
+                .ident
+                .as_ref()
+                .ok_or_else(|| syn::Error::new(f.span(), "unsupported field name"))?;
 
-        if looks_like_option(&f.ty) {
-            let x = format_ident!("__zeek_websocket_derive__impl_value_from_{name}__{field_name}");
-            quote! {
-                match value.#field_name {
-                    Some(#x) => ::zeek_websocket_types::Value::from(#x),
-                    None => ::zeek_websocket_types::Value::None,
+            Ok(if looks_like_option(&f.ty)? {
+                let x =
+                    format_ident!("__zeek_websocket_derive__impl_value_from_{name}__{field_name}");
+                quote! {
+                    match value.#field_name {
+                        Some(#x) => ::zeek_websocket_types::Value::from(#x),
+                        None => ::zeek_websocket_types::Value::None,
+                    }
                 }
-            }
-        } else {
-            quote! { ::zeek_websocket_types::Value::from(value.#field_name) }
-        }
-    });
+            } else {
+                quote! { ::zeek_websocket_types::Value::from(value.#field_name) }
+            })
+        })
+        .collect::<syn::Result<Vec<_>>>()?;
 
-    quote! {
+    Ok(quote! {
         impl From<#name> for ::zeek_websocket_types::Value {
             fn from(value: #name) -> Self {
                 Self::from(::std::vec::Vec::<::zeek_websocket_types::Value>::from([#(#fields), *]))
             }
         }
-    }
+    })
 }
 
-fn impl_from_value(name: &Ident, fields: &[&Field]) -> TokenStream {
+fn impl_from_value(name: &Ident, fields: &[&Field]) -> syn::Result<TokenStream> {
     let xs = format_ident!("__zeek_websocket_derive__impl_from_value_{name}");
 
     // Validate that all fields are named so we can cleanly unwrap below.
-    if fields.iter().any(|f| f.ident.is_none()) {
-        abort!(name.span(), "unnamed fields are unsupported");
+    if let Some(f) = fields.iter().find(|f| f.ident.is_none()) {
+        return Err(syn::Error::new(f.span(), "unnamed fields are unsupported"));
     }
 
-    let fields = fields.iter().map(|f| {
-        let field_name = f.ident.as_ref().unwrap();
+    let fields = fields
+        .iter()
+        .map(|f| {
+            let field_name = f.ident.as_ref().unwrap();
 
-        let init = quote! {
-            #xs
-                .next()
-                .unwrap_or(::zeek_websocket_types::Value::None)
-        };
+            let init = quote! {
+                #xs
+                    .next()
+                    .unwrap_or(::zeek_websocket_types::Value::None)
+            };
 
-        let x = format_ident!("__zeek_websocket_derive__impl_from_value_{name}__{field_name}");
+            let x = format_ident!("__zeek_websocket_derive__impl_from_value_{name}__{field_name}");
 
-        (
-            field_name.clone(),
-            if looks_like_option(&f.ty) {
-                quote! {
-                    let #field_name = match #init {
-                        ::zeek_websocket_types::Value::None => None,
-                        #x => Some(#x.try_into()?),
-                    };
-                }
-            } else {
-                quote! { let #field_name = #init.try_into()?; }
-            },
-        )
-    });
+            Ok((
+                field_name.clone(),
+                if looks_like_option(&f.ty)? {
+                    quote! {
+                        let #field_name = match #init {
+                            ::zeek_websocket_types::Value::None => None,
+                            #x => Some(#x.try_into()?),
+                        };
+                    }
+                } else {
+                    quote! { let #field_name = #init.try_into()?; }
+                },
+            ))
+        })
+        .collect::<syn::Result<Vec<_>>>()?;
 
-    let (names, inits): (Vec<_>, Vec<_>) = fields.unzip();
+    let (names, inits): (Vec<_>, Vec<_>) = fields.into_iter().unzip();
 
-    quote! {
+    Ok(quote! {
         impl TryFrom<::zeek_websocket_types::Value> for #name {
             type Error = ::zeek_websocket_types::ConversionError;
 
@@ -240,18 +263,18 @@ fn impl_from_value(name: &Ident, fields: &[&Field]) -> TokenStream {
                 Ok(#name { #(#names),* })
             }
         }
-    }
+    })
 }
 
 // Helper function to detect whether a type is likely an instance of `Option`.
-fn looks_like_option(ty: &Type) -> bool {
+fn looks_like_option(ty: &Type) -> syn::Result<bool> {
     let Type::Path(p) = ty else {
-        abort!(ty.span(), "unsupported type");
+        return Err(syn::Error::new(ty.span(), "unsupported type"));
     };
 
-    p.path
+    Ok(p.path
         .segments
         .last()
         .map(|ty| ty.ident == "Option")
-        .unwrap_or_default()
+        .unwrap_or_default())
 }
